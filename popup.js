69c35b4bf1c123f98197c7d765e871c0b94676f4
Harroche <119439246+Harroche@users.noreply.github.com>
2023-08-30 12:52:17 +0100

    Added Time function

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
     const generateButton = document.getElementById("generateButton");
     generateButton.addEventListener("click",function(){
-        const fileContent = "This is the content of the generated file.";
+        const fileContent = "This is empty text";
         chrome.runtime.sendMessage({ action: "generateFile", fileContent });
     });
 });
--- review_history.js
+++ review_history.js
@@ -1,20 +1,32 @@
 // This code executes when the popup is loaded
-const historyArr =[];
+const historyArr =["     TIME      :	LINK"];
 
-chrome.history.onVisited.addListener(function(result){
-  historyArr.push(result.url);
+chrome.history.onVisited.addListener(function(history){
+  historyArr.push(dateString()+"\t\t"+history.url);
 });
 console.log("Started viewing history");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    
   if (request.action === "generateFile")
   {
-      const reader= new FileReader();
-      const blob = new Blob([historyArr.join("\n"+"\n")],{ type: 'plain/text'});
-      reader.onload = function(event){
-          const dataUrl = event.target.result;
-          console.log(dataUrl);
-      };
-      console.log(reader.readAsDataURL(blob)+ " thing");
+    const reader= new FileReader();
+    const blob = new Blob([historyArr.join("\n"+"\n")],{ type: 'plain/text'});
+    reader.onload = function(event){
+      const dataUrl = event.target.result;
+      console.log(dataUrl);
+    };
+    console.log(reader.readAsDataURL(blob)+ " thing");
   }
 });
+
+//returns a string of the date and time
+function dateString(){
+  const date = new Date();
+  let str = date.getDate().toString()+"/";
+  str+= date.getMonth().toString()+"/";
+  str+= date.getFullYear().toString() +" ";
+  str+= date.getHours().toString()+":";
+  let min=date.getMinutes().toString();
+  if(min.length==1)
+      min = "0"+min;
+  return str+min;
+}
