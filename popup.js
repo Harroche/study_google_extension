50c0de5d0da0e8899bfe2ff38ab3db2dcbb6cb83
Harroche <119439246+Harroche@users.noreply.github.com>
2023-08-30 16:10:42 +0100

    Have a on and off switch

--- popup.js
+++ popup.js
@@ -1,7 +1,18 @@
+const genSwitch = document.getElementById("historySwitch");
+
+chrome.storage.sync.get("isChecked",function(data){
+    genSwitch.checked = data.isChecked;
+});
+
 document.addEventListener("DOMContentLoaded",function(){
     const generateButton = document.getElementById("generateButton");
     generateButton.addEventListener("click",function(){
-        const fileContent = "This is empty text";
-        chrome.runtime.sendMessage({ action: "generateFile", fileContent });
+        chrome.runtime.sendMessage({ action: "generateFile" });
     });
+});
+
+genSwitch.addEventListener("change",function(){
+    const isChecked = genSwitch.checked;
+    chrome.storage.sync.set({ isChecked: isChecked });
+    chrome.runtime.sendMessage({ action: "invert"});
 });
--- review_history.js
+++ review_history.js
@@ -1,25 +1,32 @@
 // This code executes when the popup is loaded
 const historyArr =["     TIME      :	LINK"];
+let running = false;
 
 chrome.history.onVisited.addListener(function(history){
-  historyArr.push(dateString()+"\t\t"+history.url);
+  if(running)  
+    historyArr.push(dateString()+"\t\t"+history.url);
 });
 console.log("Started viewing history");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "generateFile")
   {
-    const reader= new FileReader();
-    const blob = new Blob([historyArr.join("\n"+"\n")],{ type: 'plain/text'});
-    reader.onload = function(event){
-      const dataUrl = event.target.result;
-      const link = document.createElement('a');
-      link.href=dataUrl;
-      link.download="download link";
-      link.click();
-      console.log(dataUrl);
-    };
-    console.log(reader.readAsDataURL(blob)+ " thing");
+    if(running)
+    {
+      const reader= new FileReader();
+      const blob = new Blob([historyArr.join("\n"+"\n")],{ type: 'plain/text'});
+      reader.onload = function(event){
+        const dataUrl = event.target.result;
+        const link = document.createElement('a');
+        link.href=dataUrl;
+        link.download="download link";
+        link.click();
+        console.log(dataUrl);
+      };
+      console.log(reader.readAsDataURL(blob)+ " thing");
+    }
   }
+  else if(request.action === "invert")
+    invert();
 });
 
 //returns a string of the date and time
@@ -34,3 +41,13 @@ function dateString(){
       min = "0"+min;
   return str+min;
 }
+
+function invert(){
+  running=!running;
+  if(!running)
+  {
+    historyArr.length=0;
+    historyArr.push("     TIME      :	LINK");
+  }
+}
+
