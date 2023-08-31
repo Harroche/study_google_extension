d8379fb9da9590353d876a56d168a56ec0e06762
Harroche <119439246+Harroche@users.noreply.github.com>
2023-08-31 11:39:54 +0100

    Updated css to denote recording

--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
 const genSwitch = document.getElementById("historySwitch");
+const colouredText=document.getElementById("recording");
+
+
+chrome.storage.sync.get("colorPresence",function(data){
+    if(data.colorPresence)
+        colouredText.style.color = data.colorPresence;
+});
 
 chrome.storage.sync.get("isChecked",function(data){
     genSwitch.checked = data.isChecked;
@@ -13,9 +20,13 @@ document.addEventListener("DOMContentLoaded",function(){
 
 genSwitch.addEventListener("change",function(){
     const isChecked = genSwitch.checked;
-    const colouredText=document.getElementById("recording");
     colouredText.style.color= (isChecked)? "red":"white";
+    (isChecked)? saveColour("red"):saveColour("white");
     chrome.storage.sync.set({ isChecked: isChecked });
     chrome.runtime.sendMessage({ action: "invert"});
 });
+
+function saveColour(colour){
+    chrome.storage.sync.set({"colorPresence":colour});
+}
 
